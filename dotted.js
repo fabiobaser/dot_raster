46d8050476c5b8208981ab65baea193ad0aac65a
Fabio Baser <f.baser@monday-consulting.com>
2020-05-29 13:12:48 +0200

    Init

---
+++ dotted.js
@@ -0,0 +1,106 @@
+/** @format */
+
+const canvasSketch = require("canvas-sketch")
+const {
+  renderPaths,
+  createPath,
+  pathsToPolylines
+} = require("canvas-sketch-util/penplot")
+const { clipPolylinesToBox } = require("canvas-sketch-util/geometry")
+const Random = require("canvas-sketch-util/random")
+const chunks = require("./chunks.js")
+
+const RENDER_CIRCLES = true
+const RENDER_BOX = false
+
+console.log("Random Seed:", Random.getSeed())
+
+const settings = {
+  dimensions: "A4",
+  orientation: "portrait",
+  pixelsPerInch: 300,
+  scaleToView: true,
+  units: "cm"
+}
+
+const TWO_PI = Math.PI * 2
+
+const sketch = (props) => {
+  const { width, height, units } = props
+  console.log(width, height)
+  const paths = []
+  console.log(chunks.length)
+
+  RENDER_BOX &&
+    paths.push(
+      createPath((ctx) => {
+        ctx.rect(0.5, 0.5, width - 1, height - 1)
+      })
+    )
+
+  RENDER_CIRCLES &&
+    chunks.slice(0, 20000).forEach((chunk) => {
+      let [x, y, l] = chunk
+      x = x / 29 + 0.4
+      y = y / 30 + 0.75
+
+      const s = 1 - l
+
+      console.log(s)
+
+      //if (s < 0.35) return
+
+      paths.push(
+        createPath((ctx) => {
+          ctx.arc(x, y, s * 0.14, 0, TWO_PI)
+        })
+      )
+
+      if (s > 1 - 0.15) {
+        paths.push(
+          createPath((ctx) => {
+            ctx.arc(x, y, (1 - l) * 0.06, 0, TWO_PI)
+          })
+        )
+
+        paths.push(
+          createPath((ctx) => {
+            ctx.arc(x, y, (1 - l) * 0.02, 0, TWO_PI)
+          })
+        )
+      }
+
+      // if (s > 0.6) {
+      //   paths.push(
+      //     createPath((ctx) => {
+      //       ctx.arc(x, y, (1 - l) * 0.09, 0, TWO_PI)
+      //     })
+      //   )
+      // }
+
+      // if (s > 0.4) {
+      //   paths.push(
+      //     createPath((ctx) => {
+      //       ctx.arc(x, y, (1 - l) * 0.04, 0, TWO_PI)
+      //     })
+      //   )
+      // }
+    })
+
+  let lines = pathsToPolylines(paths, { units })
+  console.log("%cNumber of Polylines: ", "color: orange", lines.length)
+
+  const box = [0.9, 0.92, width - 1.1, height - 1.15]
+  lines = clipPolylinesToBox(lines, box)
+
+  return (props) =>
+    renderPaths(lines, {
+      ...props,
+      lineJoin: "round",
+      lineCap: "round",
+      lineWidth: 0.08,
+      optimize: false
+    })
+}
+
+canvasSketch(sketch, settings)
---
+++ genRasterizedImg.js
@@ -0,0 +1,69 @@
+/** @format */
+
+const canvasSketch = require("canvas-sketch")
+
+const settings = {
+  dimensions: "A4"
+}
+
+const sketch = ({ context, width, height }) => {
+  console.log("%cSettings: ", "color: orange", settings, { width, height })
+
+  const img = document.createElement("img")
+  img.setAttribute("src", "./orig.jpg")
+
+  return ({ context, width, height }) => {
+    context.drawImage(img, 0, 0)
+
+    const chunkSize = 10
+    const chunks = []
+    for (let x = 0; x < width; x += chunkSize) {
+      for (let y = 0; y < height; y += chunkSize) {
+        const imageData = context.getImageData(x, y, chunkSize, chunkSize)
+        const pixelComponents = imageData.data
+        const pixels = []
+        for (let i = 0; i < pixelComponents.length; i += 4) {
+          const l = pixelComponents[i]
+          pixels.push(l)
+        }
+
+        const l = pixels.reduce((prev, curr) => (curr += prev)) / pixels.length
+
+        const chunk = [x, y, l / 255]
+        chunks.push(chunk)
+      }
+    }
+
+    window.download = () => {
+      download("chunks.json", JSON.stringify(chunks))
+
+      function download(filename, text) {
+        var pom = document.createElement("a")
+        pom.setAttribute(
+          "href",
+          "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+        )
+        pom.setAttribute("download", filename)
+
+        if (document.createEvent) {
+          var event = document.createEvent("MouseEvents")
+          event.initEvent("click", true, true)
+          pom.dispatchEvent(event)
+        } else {
+          pom.click()
+        }
+      }
+    }
+
+    chunks.forEach((chunk) => {
+      let [x, y, l] = chunk
+      l *= 255
+      context.fillStyle = `rgb(${l},${l},${l})`
+      context.fillRect(x, y, chunkSize, chunkSize)
+    })
+
+    console.log(chunks)
+  }
+}
+
+canvasSketch(sketch, settings)
